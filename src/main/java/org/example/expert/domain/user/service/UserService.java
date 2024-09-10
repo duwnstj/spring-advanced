d28c23663b4edbff960c533dfe6ac79a8b532f53
여준서 <duwnstj12@naver.com>
2024-09-10 21:44:32 +0900

    레벨 1-3 : 메서드 분리

--- src/main/java/org/example/expert/domain/user/service/UserService.java
+++ src/main/java/org/example/expert/domain/user/service/UserService.java
@@ -23,13 +23,21 @@ public class UserService {
         return new UserResponse(user.getId(), user.getEmail());
     }
 
-    @Transactional
-    public void changePassword(long userId, UserChangePasswordRequest userChangePasswordRequest) {
-        if (userChangePasswordRequest.getNewPassword().length() < 8 ||
-                !userChangePasswordRequest.getNewPassword().matches(".*\\d.*") ||
-                !userChangePasswordRequest.getNewPassword().matches(".*[A-Z].*")) {
+    // 유효성 검사
+    private void validateChangePassword(UserChangePasswordRequest userChangePasswordRequest) {
+        String pw = userChangePasswordRequest.getNewPassword();
+        if (pw.length() < 8 ||
+                !pw.matches(".*\\d.*") ||
+                !pw.matches(".*[A-Z].*")) {
             throw new InvalidRequestException("새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.");
         }
+    }
+
+    @Transactional
+    public void changePassword(long userId, UserChangePasswordRequest userChangePasswordRequest) {
+
+        validateChangePassword(userChangePasswordRequest);
+
 
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new InvalidRequestException("User not found"));
@@ -44,4 +52,6 @@ public class UserService {
 
         user.changePassword(passwordEncoder.encode(userChangePasswordRequest.getNewPassword()));
     }
+
+
 }
